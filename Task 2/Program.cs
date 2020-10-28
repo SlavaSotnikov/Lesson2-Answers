8796c3685b90e071235d5a2e7b57438406b9b254
Slava Sotnikov <SlavaSotnikov2020@gmail.com>
2020-10-28 06:03:47 +0200

    Add project files.

---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Converter converter = new Converter(8.56, 10.14, 0.12);
+           
+            converter.ToUsd(100);
+            converter.FromUsd(100);
+
+            Console.ReadKey();
+        }
+    }
+}
